8233e73d4232c774041ca69d30f93d57e9e515fa
Nathan N <Nitharios@users.noreply.github.com>
2017-08-22 23:16:56 -1000

    Cleaned up code

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -11,436 +11,192 @@ global.bruhdash = {
 
   // returns the first element of an array
   first: function (arr) {
-
-      if (Array.isArray(arr)) {
-
-        return arr[0]
-
-      } else {
-
-        return 'Not an array!'
-      }
+    return arr[0]
   },
 
   // returns the last element of an array
   last: function (arr) {
-
-    if (Array.isArray(arr)) {
-
-      return arr[arr.length-1]
-
-    } else {
-
-      return 'Not an array!'
-    }
+    return arr[arr.length-1]
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function (arr, search) {
-
-    if (Array.isArray(arr)) {
-
-      for (var i = 0; i < arr.length; i++) {
-
-        if (arr[i] === search) {
-
-          return i
-        }
-      } 
-
-      return -1
-
-    } else {
-
-      return 'Not an array!'
-    }
-  },
-
-  // returns the index of the first matching element from left to right
-  lastIndexOf: function (arr, search) {
-
-    if (Array.isArray(arr)) {
-
-      for (var i = arr.length; i >= 0; i--) {
-
-        if (arr[i] === search) {
-
-          return i
-        }
+  indexOf: function (arr, match) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === match) {
+        return i
       }
+    }
+    return -1
+  },
 
-      return -1
-
-    } else {
-
-      return 'Not an array!'
-     }
+  // returns the index of the first matching element from right to left
+  lastIndexOf: function (arr, match) {
+    for (var i = arr.length-1; i > 0; i--) {
+      if (arr[i] === match) {
+        return i
+      }
+    }
+    return -1
   },
 
   // returns an array with all elements except for the last element
   initial: function (arr) {
-
-    if (Array.isArray(arr)) {
-
-      var arrDash = []
-
-      for (var i = 0; i < arr.length-1; i++) {
-
-        arrDash[i] = arr[i]
-
-      }
-
-      return arrDash
-
-    } else {
-
-      return 'Not an array!'
-    }
+    var popArr = arr
+      popArr.pop()                       // destructive
+    return popArr                      
   },
   
   // returns an array with all falsey values removed
   compact: function(arr) {
+    var truthyArr = []
 
-    if (Array.isArray(arr)) {
-
-      var arrDash = []
-
-      for (var i = 0; i < arr.length; i++) {
-
-        if (typeof arr[i] === 'number' && arr[i] > 0) {
-
-            arrDash[arrDash.length] = arr[i]
-
-        }
+    for (var i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === 'number' && arr[i] > 0) {
+        truthyArr.push(arr[i])
       }
-
-      return arrDash
-
-    } else {
-
-      return 'Not an array!'
     }
+    return truthyArr
   },
 
   // creates a slice of an array from the start index up to but not including the end index
   slice: function (arr, start, end) {
+    var sliceArr = []
 
-    var arrDash = []
-
-    if (Array.isArray(arr)) {
-
-      for (var i = start; i < end; i++) {
-
-        arrDash[arrDash.length] = arr[i]
-      }
-
-      return arrDash
-
-    } else {
-
-      return 'Not an array!'
+    for (var i = start; i < end; i++) {
+      sliceArr.push(arr[i])
     }
+
+    return sliceArr
   },
 
   // returns a slice of array with n elements dropped from the beginning
-  drop: function(arr, drop){
+  drop: function(arr, drop) {
+    var dropArr = []
 
-    var arrDash = []
-
-    if (Array.isArray(arr)) {
-
-      if (drop === undefined) {
-
-        for (var i = 1; i < arr.length; i++) {
-
-          arrDash[arrDash.length] = arr[i]
-
-        } 
-
-        return arrDash
-
-      } else if (drop > 0) {
-
-        for (var i = drop; i < arr.length; i++) {
-
-          arrDash[arrDash.length] = arr[i]
-        }
-
-        return arrDash
-
-      } else if (drop === 0) {
-
-        return arr
-
-      }
-
+    if (drop === undefined) {
+      dropArr = arr
+        dropArr.shift()
     } else {
-
-      return 'Not an array!'
-
+      for (var i = drop; i < arr.length; i++) {
+        dropArr.push(arr[i])
+      }
     }
+    return dropArr
   },
 
   // returns a slice of array with n elements dropped from the end
   dropRight: function(arr, drop) {
+    var dropArr = []
 
-    var arrDash = []
-
-    if (Array.isArray(arr)) {
-
-      if (drop === undefined) {
-
-        for (var i = 0; i < arr.length-1; i++) {
-
-          arrDash[arrDash.length] = arr[i]
-
-        } 
-
-        return arrDash
-
-      } else if (drop > 0) {
-
-        for (var i = 0; i < arr.length-drop; i++) {
-
-          arrDash[arrDash.length] = arr[i]
-        }
-
-        return arrDash
-
-      } else if (drop === 0) {
-
-        return arr
-
-      }
-
+    if (drop === undefined) {
+      dropArr = arr
+        dropArr.pop()
     } else {
-
-      return 'Not an array!'
-
+      for (var i = 0; i < arr.length-drop; i++) {
+        dropArr.push(arr[i])
+      }
     }
-
+    return dropArr
   },
 
   // creates a slice of an array with n elements taken from the beginning
-  take: function (arr, slice) {
+  take: function (arr, take) {
+    var takeArr = []
 
-    var arrDash = []
-
-    if (Array.isArray(arr)) {
-
-      if (slice === undefined) {
-
-        arrDash[arrDash.length] = arr[0]
-        return arrDash
-
-      } else if (slice > 0 && slice <= arr.length) {
-
-        for (var i = 0; i < slice; i++) {
-
-          arrDash[arrDash.length] = arr[i]
-
-        }
-
-        return arrDash
-
-      } else if (slice > arr.length) {
-
-        return arr
-
-      } else if (slice === 0) {
-
-        return arrDash
-
+    if (take === undefined) {
+      takeArr.push(arr[0])
+    } else if (take >= arr.length) {
+      takeArr = arr
+    } else if (take > 0 && take < arr.length) {
+      for (var i = 0; i < take; i++) {
+        takeArr.push(arr[i])
       }
-
-    } else {
-
-      return 'Not an array!'
-
-    }
+    } 
+    return takeArr
   },
 
   // creates a slice of an array with n elements taken from the end
-  takeRight: function (arr, slice) {
+  takeRight: function (arr, take) {
+    var takeArr = []
 
-    var arrDash = []
-
-    if (Array.isArray(arr)) {
-
-      if (slice === undefined) {
-
-        arrDash[arrDash.length] = arr[arr.length-1]
-        return arrDash
-
-      } else if (slice > 0 && slice <= arr.length) {
-
-        for (var i = slice-1; i < arr.length; i++) {
-
-          arrDash[arrDash.length] = arr[i]
-
-        }
-
-        return arrDash
-
-      } else if (slice > arr.length) {
-
-        return arr
-
-      } else if (slice === 0) {
-
-        return arrDash
-
+    if (take === undefined) {
+      takeArr.push(arr[arr.length-1])
+    } else if (take >= arr.length) {
+      takeArr = arr
+    } else if (take > 0 && take < arr.length) {
+      for (var i = take-1; i < arr.length; i++) {
+        takeArr.push(arr[i])
       }
-
-    } else {
-
-      return 'Not an array!'
-
     }
+    return takeArr
   },
 
   // fills elements of array with specified value from the start index
   // up to but not including the end index
-  fill: function(arr, char, start, end) {
+  fill: function(arr, value, start, end) {
+    var fillArr = []
 
-    if (Array.isArray(arr)) {
-
-      if (start === undefined && end === undefined) {
-
-        for (var i = 0; i < arr.length; i++) {
-
-          arr[i] = char
-
-        }
-
-      } else {
-
-        for (var i = start; i < end; i++) {
-
-          arr[i] = char
-
-        }
+    if (start === undefined && end === undefined) {
+      for (var i = 0; i < arr.length; i++) {
+        fillArr.push(value)
       }
-
-      return arr
-
     } else {
-
-      return 'Not an array!'
-
+      fillArr = arr
+      for (var i = 0; i < arr.length; i++) {
+        if (i >= start && i < end) {
+          fillArr.splice(i, 1, value)
+        }
+      } 
     }
+    return fillArr
   },
 
   // removes all given values from an array
-  pull: function (arr, valOne, valTwo) {
-
+  pull: function (arr, valueOne, valueTwo) {
     var arrDash = []
 
-    if (Array.isArray(arr)) {
-
-      for (var i = 0; i < arr.length; i++) {
-
-        if (arr[i] !== valOne && arr[i] !== valTwo) {
-
-          arrDash[arrDash.length] = arr[i]
-
-        }
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] !== valueOne && arr[i] !== valueTwo) {
+        arrDash.push(arr[i])
       }
-
-      return arrDash
-
-    } else {
-
-      return 'Not an array!'
-
     }
+    return arrDash
   },
 
   // removes elements of an array corresponding to the given indices
   pullAt: function (arr, idx) {
-
     var arrDash = []
 
     for (var i = 0; i < arr.length; i++) {
-
-      var match = false
-
-      for (var j = 0; j < idx.length; j++) {
-
-        if (i === idx[j]) {
-
-          match = true
-          break;
-
-        }
-      }
-
-      if (match !== true) {
-
-        arrDash[arrDash.length] = arr[i]
-
+      if (idx.indexOf(i) === -1) {
+        arrDash.push(arr[i])
       }
     }
-
     return arrDash
-
   },
 
   // creates an array excluding all the specified values
   without: function(arr, idx) {
-
     var arrDash = []
 
     for (var i = 0; i < arr.length; i++) {
-
-      var match = false
-
-      for (var j = 0; j < idx.length; j++) {
-
-        if (arr[i] === idx[j]) {
-
-          match = true
-          break;
-
-        }
-      }
-
-      if (match !== true) {
-
-        arrDash[arrDash.length] = arr[i]
-
+      if (idx.indexOf(arr[i]) === -1) {
+        arrDash.push(arr[i])
       }
     }
-
     return arrDash
   },
 
   // returns an array with specified values excluded
-  difference: function(arr, idx) {
-
+  difference: function(arrOne, arrTwo) {
     var arrDash = []
 
-
-    for (var i = 0; i < arr.length; i++) {
-
-      for (var j = 0; j < idx.length; j++) {
-
-        if (arr[i] === idx[j]) {
-
-          break
-
-        }
-      }
-
-      if (j === idx.length) {
-
-        arrDash[arrDash.length] = arr[i]
-
+    for (var i = 0; i < arrOne.length; i++) {
+      if (arrTwo.indexOf(arrOne[i]) === -1) {
+        arrDash.push(arrOne[i])
       }
     }
-
     return arrDash
-
   },
 
   /*******************
@@ -449,183 +205,93 @@ global.bruhdash = {
 
   // creates an array of grouped elements
   zip: function (arrOne, arrTwo) {
-
-    var arrDash = []
+    var zipArr = []
 
     for (var i = 0; i < arrOne.length; i++) {
-
-      arrDash[arrDash.length] = [arrOne[i], arrTwo[i]]
-
+      var tempArr = []
+        tempArr.push(arrOne[i], arrTwo[i])
+      zipArr.push(tempArr)
     }
-
-    return arrDash
-
+    return zipArr
   },
 
   // creates an array of grouped elements in their pre-zip configuration
   unzip: function (arr) {
-
+    var unzipArr = []
     var arrOne = []
     var arrTwo = []
-    var arrDash = []
 
     for (var i = 0; i < arr.length; i++) {
-
-      arrOne[arrOne.length] = arr[i][0]
-      arrTwo[arrTwo.length] = arr[i][1]
-
+      arrOne[i] = arr[i][0]
+      arrTwo[i] = arr[i][1]
     }
-
-    arrDash = [arrOne, arrTwo]
-
-    return arrDash
-
+    unzipArr.push(arrOne, arrTwo)
+    return unzipArr
   },
 
   // creates an array of elements into groups of length of specified size
-  chunk: function(arr, val){
-
-    var arrDash = []
-
-    if (arr.length === 0 || val === 0) {
-
-      return arrDash
-
-    } else if (val >= arr.length) {
-
-      arrDash = [arr]
-      return arrDash
-
+  chunk: function(arr, size){
+    var chunkArr = []
+    if (arr.length === 0 || size === 0) {
+      return chunkArr
     } else {
-
-      for (var i = 0; i < arr.length; i++) {
-
-        var tempArr = []
-
-        for (var j = 0; j < val; j++) {
-
-          tempArr[j] = arr[i]
-          i++
-
-          if (i === arr.length) {
-
-            break
-
+      for (var i = 0; i < arr.length;) {
+      var tempArr = []
+        for (var j = 0; j < size; j++) {
+          if (arr[i]) {
+            tempArr.push(arr[i])
+            i++
           }
         }
-
-        i--
-
-        arrDash[arrDash.length] = tempArr
-
+      chunkArr.push(tempArr)
       }
     }
-
-    return arrDash
-
+    return chunkArr
   },
 
   // iterates over elements of a collection and invokes iteratee for each element
   // Note: this should work for arrays and objects
   forEach: function(collection, iteratee) {
-
-    var value;
-
-    if (typeof collection === 'object' && !Array.isArray(collection)) {
-      
-      var keys = Object.keys(collection)
-      var values = Object.values(collection) 
-
-      for (var i = 0; i < values.length; i++) {
-
-        value = values[i]
-        iteratee(value, i)
-
-      }
-
-    } else if (Array.isArray(collection)) {
-
-      for (var i = 0; i < collection.length; i++) {
-
-        value = collection[i]
-        iteratee(value, i)
-
-      }
+    for (var i in collection) {
+      iteratee(collection[i])
     }
   },
 
   // creates an array of values by running each element in collection thru the iteratee
   // Note: this should work for arrays and objects
   map: function(collection, iteratee) {
-
     var arrDash = []
-    var value;
-
-    if (typeof collection === 'object' && !Array.isArray(collection)) {
-      
-      var keys = Object.keys(collection)
-      var values = Object.values(collection) 
-
-      for (var i = 0; i < values.length; i++) {
-
-        value = values[i]
-        arrDash[arrDash.length] = iteratee(value, i)
-
-      }
-
-    } else if (Array.isArray(collection)) {
-
-      for (var i = 0; i < collection.length; i++) {
-
-        value = collection[i]
-        arrDash[arrDash.length] = iteratee(value, i)
-
-      }
+    for (var i in collection) {
+      arrDash.push(iteratee(collection[i]))
     }
-
     return arrDash
   },
 
-  /***********************
+  /*************************
    *  SUPER STRETCH GOALS!  *
    *************************/ 
 
   // iterates over elements of a collection and returns all elements that the predicate returns truthy for
   // Note: this should work for arrays and objects
   filter: function(collection, iteratee) {
-
     var arrDash = []
     var objDash = {}
 
-    if (typeof collection === 'object' && !Array.isArray(collection)) {
-      
-      var keys = Object.keys(collection)
-      var values = Object.values(collection) 
-
-      for (var i = 0; i < values.length; i++) {
-
-        if (iteratee(values[i]) === true) {
-
-          objDash[keys[i]] = values[i]
-
+    if (Array.isArray(collection)) {
+      for (var i in collection){
+        if (iteratee(collection[i])) {
+          arrDash.push(collection[i])
         }
       }
-
-      return objDash
-
-    } else if (Array.isArray(collection)) {
-
-      for (var i = 0; i < collection.length; i++) {
-
-        if (iteratee(collection[i]) === true) {
-
-          arrDash[arrDash.length] = collection[i]
-
-        }
-      }
-
       return arrDash
 
+    } else if (!Array.isArray(collection)) {
+      for (var j in collection) {
+        if (iteratee(collection[j])) {
+          objDash[j] = collection[j]
+        }
+      }
+      return objDash
     }
   },
 
@@ -633,30 +299,11 @@ global.bruhdash = {
   // in the collection through an iteratee
   // Note: this should work for arrays and objects
   reduce: function(collection, iteratee) {
+    var sum = 0
 
-    var results = 0
-
-    if (typeof collection === 'object' && !Array.isArray(collection)) {
-      
-      var keys = Object.keys(collection)
-      var values = Object.values(collection) 
-
-      for (var i = 0; i < values.length; i++) {
-
-        results = iteratee(results, values[i])
-
-      }
-
-    } else if (Array.isArray(collection)) {
-
-      for (var i = 0; i < collection.length; i++) {
-
-        results = iteratee(results, collection[i])
-
-      }
+    for (var i in collection) {
+      sum = iteratee(sum, collection[i])
     }
-
-  return results
-
+    return sum
   }
 };
